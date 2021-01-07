cd85cb550d68deb4c20f4caf4e15b881693e391c
EC2 Default User <ec2-user@ip-192-168-40-241.ec2.internal>
2021-01-07 22:00:27 +0000

    Got the token validation working correctly

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,6 @@
 
 use std::collections::BTreeMap;
 use std::convert::TryInto;
-use rustls::ServerCertVerifier;
-use webpki::ECDSA_P384_SHA384;
-use serde::Serialize;
-use serde_cbor::Serializer;
-
-use byteorder::{ BigEndian, WriteBytesExt};
 
 // The AWS Nitro Attestation Document.
 // This is described in 
@@ -39,13 +33,15 @@ pub struct NitroToken {
 
 impl NitroToken {
     pub fn authenticate_token(token_data: &Vec<u8>, trusted_root_cert: &[u8]) -> Result<AttestationDocument, String> {
-        println!("NitroToken::authenticate_token started");
-        let (mut protected, mut payload, mut signature) = NitroToken::parse_token(token_data)
+        // Following the steps here: https://docs.aws.amazon.com/enclaves/latest/user/verify-root.html
+        // Step 1. Decode the CBOR object and map it to a COSE_Sign1 structure
+        let (_protected, payload, _signature) = NitroToken::parse_token(token_data)
             .map_err(|err| format!("NitroToken::authenticate_token parse_token failed:{:?}", err))?;
+        // Step 2. Exract the attestation document from the COSE_Sign1 structure
         let document = NitroToken::parse_payload(&payload)
             .map_err(|err| format!("NitroToken::authenticate_token parse_attestation_document failed:{:?}", err))?;
 
-        // first things first, check the validity of the certificate chain.
+        // Step 3. Verify the certificate's chain
         let mut certs: Vec<rustls::Certificate> = Vec::new();
         let cert = rustls::Certificate(document.certificate.clone());
         certs.push(cert);
@@ -62,71 +58,39 @@ impl NitroToken {
 
 
         let verifier = rustls::AllowAnyAuthenticatedClient::new(root_store);
-        let verified = verifier.verify_client_cert(&certs)
-            .map_err(|err| format!("NitroToken::authenticate_token verify_client_cert failed:{:?}", err))?; 
+        let _verified = verifier.verify_client_cert(&certs)
+            .map_err(|err| format!("NitroToken::authenticate_token verify_client_cert failed:{:?}", err))?;
+        // if verify_client_cert didn't generate an error, authentication passed
 
-        let mut manually_serialized: Vec<u8> = Vec::new();
-
-        manually_serialized.push(0x84); // An array with 4 elements
-
-        // element # 1: The Context, a text string
-        let context: String = "Signature1".to_string();
-        let mut serialized_context: Vec<u8> = serde_cbor::to_vec(&context).unwrap();
-        manually_serialized.append(&mut serialized_context);
-
-        println!("protected:{:02x?}", protected); 
-        // Element #2: The protected attributed from the body structure, encoded as a bstr
-        //let mut body_protected: std::collections::BTreeMap<i32, i32> = std::collections::BTreeMap::new();
-        //body_protected.insert(1, -35);
-        //let mut serialized_protected: Vec<u8> = serde_cbor::to_vec(&body_protected).unwrap();
-        manually_serialized.push(0x44); // bstr, length 4
-        manually_serialized.append(&mut protected.clone());
-
-        // Element #3 is omitted for COSE_Sign1
-
-        // Element # 4: The protected attributes from the application, encoded in a bstr.
-        // If this field is not supplied (which it ain't for us), it defaults to a zero-length
-        // binary string
-        manually_serialized.push(0x40);  // empty bstr
-
-        // Element #5: The payload to be signed encoded in a bstr type
-        // The payload (encoded as a Bstr)
-        manually_serialized.push(0x59); // bstr, with 2 bytes for length
-        // now add the two bytes for the length
-        let len: u16 = payload.len() as u16;
-        let mut len_vec = vec![];
-        len_vec.write_u16::<BigEndian>(len).unwrap();
-        manually_serialized.append(&mut len_vec);
-        // now add the payload itself
-        manually_serialized.append(&mut payload.clone());
-        //println!("manually_serialized:{:02x?}", manually_serialized);
-
-        // This is the ToBeSigned structure described in section 4.4 of https://tools.ietf.org/html/rfc8152#appendix-C.5
-        let mut to_be_signed: Vec<u8> = Vec::new();
-        to_be_signed.push(0x59); // bstr, with 2 bytes for length
-        // add the two bytes for the length
-        let mut len_vec = vec![];
-        let len: u16 = manually_serialized.len() as u16;
-        len_vec.write_u16::<BigEndian>(len).unwrap();
-        to_be_signed.append(&mut len_vec);
-        // now add the data
-        to_be_signed.append(&mut manually_serialized);
-        //println!("to_be_signed:{:02x?}", to_be_signed);
-
-
-        println!("Received signature:{:02x?}", signature);
-
-        // let end_cert = webpki::EndEntityCert::from(&document.certificate)
-        //     .map_err(|err| format!("NitroToken::authenticate_token Failed to create EndEntityCert:{:?}", err))?;
-        // end_cert.verify_signature(
-        //     &ECDSA_P384_SHA384,
-        //     &to_be_signed,
-        //     &signature,
-        // )
-        //     .map_err(|err| format!("NitroToken::authenticate_token Failed to authenticate signature on the token:{:?}", err))?;
-
-        println!("NitroToken::authenticate_token We've faked it for now. If you see this, someone (probably Derek) made a mistake and checked in bad code. You should let Derek know.");
-        Ok(document)
+        // Step 4. Ensure the attestation document is properly signed
+        let authenticated = {
+            let sig_structure = aws_nitro_enclaves_cose::sign::COSESign1::from_bytes(token_data)
+                .map_err(|err| {
+                    format!("NitroToken::authenticate_token failed to load token_data as COSESign1 structure:{:?}", err)
+                })?;
+            let cert = openssl::x509::X509::from_der(&document.certificate)
+                .map_err(|err| {
+                    format!("NitroToken::authenticate_token failed to parse document.certificate as X509 certificate:{:?}", err)
+                })?;
+            let public_key = cert.public_key()
+                .map_err(|err| {
+                    format!("NitroToken::authenticate_token failed to extract public key from certificate:{:?}", err)
+                })?;
+            let pub_ec_key = public_key.ec_key()
+                .map_err(|err| {
+                    format!("NitroToken::authenticate_token failed to get ec_key from public_key:{:?}", err)
+                })?;
+            let result = sig_structure.verify_signature(&pub_ec_key)
+                .map_err(|err| {
+                    format!("NitroToken::authenticate_token failed to verify signature on sig_structure:{:?}", err)
+                })?;
+            result
+        };
+        if !authenticated {
+            return Err(format!("NitroToken::authenticate_token invalid COSE certificate for provided key"));
+        } else {
+            return Ok(document);
+        }
     }
 
     fn parse_token(token_data: &Vec<u8>) -> Result<(Vec<u8>, Vec<u8>, Vec<u8>), String> {
@@ -142,7 +106,7 @@ impl NitroToken {
             serde_cbor::Value::Bytes(prot) => prot,
             _ => panic!("nitro-enclave-token::parse_nitro_token Unknown field protected:{:?}", elements[0]),
         };
-        let unprotected = match &elements[1] {
+        let _unprotected = match &elements[1] {
             serde_cbor::Value::Map(unprot) => unprot,
             _ => panic!("nitro-enclave-token::parse_nitro_token Unknown field unprotected:{:?}", elements[1]),
         };
@@ -181,9 +145,8 @@ impl NitroToken {
 
         let public_key: Option<Vec<u8>> = match document_map.get(&serde_cbor::Value::Text("public_key".to_string())) {
             Some(serde_cbor::Value::Bytes(val)) => Some(val.to_vec()),
-            Some(Null) => None,
+            Some(_null) => None,
             None => None,
-            _ => return Err(format!("nitro-enclave-token::parse_attestation_document public_key is wrong type or not present")),
         };
     
         let certificate: Vec<u8> = match document_map.get(&serde_cbor::Value::Text("certificate".to_string())) {
@@ -218,8 +181,7 @@ impl NitroToken {
         let user_data: Option<Vec<u8>> = match document_map.get(&serde_cbor::Value::Text("user_data".to_string())) {
             Some(serde_cbor::Value::Bytes(val)) => Some(val.to_vec()),
             None => None,
-            Some(Null) => None,
-            _ => return Err(format!("nitro-enclave-token::parse_attestation_document user_data is wrong type or not present")),
+            Some(_null) => None,
         };
 
     
@@ -257,4 +219,6 @@ impl NitroToken {
         })
     }
 }
+
+
 
